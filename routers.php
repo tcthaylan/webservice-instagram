07d234e3c55de4356571ae84d94e06e1347fcb9e
Thaylan Conceição <thaylan.conceicao@nextpage.com.br>
2019-05-22 13:00:59 -0300

    comment routers

--- routers.php
+++ routers.php
@@ -2,17 +2,17 @@
 global $routes;
 $routes = array();
 
-$routes['/users/login'] = '/users/login';
-$routes['/users/new'] = '/users/new_record';
-$routes['/users/feed'] = '/users/feed';
-$routes['/users/{id}'] = '/users/view/:id';
-$routes['/users/{id}/photos'] = '/users/photos/:id';
-$routes['/users/{id}/follow'] = '/users/follow/:id';
+$routes['/users/login'] = '/users/login'; /*(POST)*/
+$routes['/users/new'] = '/users/new_record'; /*(POST)*/
+$routes['/users/feed'] = '/users/feed'; /*(GET)*/
+$routes['/users/{id}'] = '/users/view/:id'; /*(GET) (POST) (PUT)*/
+$routes['/users/{id}/photos'] = '/users/photos/:id'; /*(GET)*/
+$routes['/users/{id}/follow'] = '/users/follow/:id'; /*(POST) (DELETE)*/
 
-$routes['/photos/random'] = '/photos/random';
-$routes['/photos/new'] = '/photos/new_record';
-$routes['/photos/{id}'] = '/photos/view/:id';
-$routes['/photos/{id}/comment'] = '/photos/comment/:id';
-$routes['/photos/{id}/like'] = '/photos/like/:id';
+$routes['/photos/random'] = '/photos/random'; /*(GET)*/
+$routes['/photos/new'] = '/photos/new_record'; /*(POST)*/
+$routes['/photos/{id}'] = '/photos/view/:id'; /*(GET) (DELETE)*/
+$routes['/photos/{id}/comment'] = '/photos/comment/:id'; /*(POST) (DELETE)*/
+$routes['/photos/{id}/like'] = '/photos/like/:id'; /*(POST) (DELETE)*/
 
 $routes['/comments/{id}'] = '/photos/delete_comment/:id';
